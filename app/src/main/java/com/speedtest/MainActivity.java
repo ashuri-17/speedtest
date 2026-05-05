aaacf4af6a0b63bb9975f6fc28a8adb7588a2502
SpeedTest <dev@speedtest.com>
2026-05-06 02:31:38 +0800

    Minimal Java-only Speed Test app

---
+++ app/src/main/java/com/speedtest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.speedtest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Speed Test Ready");
+    }
+}
